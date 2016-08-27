c27f255bccb63d4b2ce59ba8994625e109786e65
bagrounds <bagrounds@gmail.com>
2016-08-27 11:57:15 -0700

    [REMOVE] ipv6 address from ipaddress

--- freecodecamp-request-header-parser-microservice.js
+++ freecodecamp-request-header-parser-microservice.js
@@ -36,7 +36,7 @@
     var agent = useragent.parse(request.headers['user-agent'])
 
     return {
-      ipaddress: request.connection.remoteAddress,
+      ipaddress: /:([\d\.]+)$/.exec(request.connection.remoteAddress)[1],
       language: /^(.*);/.exec(request.headers['accept-language'])[1],
       software: agent.os.family
     }
