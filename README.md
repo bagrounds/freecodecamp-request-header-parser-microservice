d30b4e4a164e9fa63a96260c023d768dbefa0817
bagrounds <bagrounds@gmail.com>
2016-08-27 11:52:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# freecodecamp-request-header-parser-microservice
+
+See requirements here: https://www.freecodecamp.com/challenges/request-header-parser-microservice
+
+## Installation
+
+``` bash
+  $ npm install 'bagrounds/freecodecamp-request-header-parser-microservice'
+```
+
+## Usage
+``` bash
+  $ npm start
+```
+
+## License
+[MIT][license-url]
+
+[license-url]: LICENSE
+
---
+++ freecodecamp-request-header-parser-microservice.js
@@ -0,0 +1,45 @@
+/**
+ * freecodecamp-request-header-parser-microservice
+ *
+ * Assignment:
+ * https://www.freecodecamp.com/challenges/request-header-parser-microservice
+ */
+;(function () {
+  'use strict'
+
+  /* imports */
+  var express = require('express')
+  var useragent = require('useragent')
+
+  var PORT = process.env.PORT || 8080
+  var app = express()
+
+  // handle dates and timestamps
+  app.get('/', function (request, response) {
+    response.json(parseHeader(request))
+  })
+
+  app.listen(PORT, function () {
+    console.log('Listening on port ' + PORT)
+  })
+
+  /**
+   * Parse the requester's ip address, language, and operating system from the
+   * http request object.
+   *
+   * @function parseHeader
+   *
+   * @param {Object} request http request
+   * @return {Object} containing ipaddress, language, and software fields
+   */
+  function parseHeader (request) {
+    var agent = useragent.parse(request.headers['user-agent'])
+
+    return {
+      ipaddress: request.connection.remoteAddress,
+      language: /^(.*);/.exec(request.headers['accept-language'])[1],
+      software: agent.os.family
+    }
+  }
+})()
+
